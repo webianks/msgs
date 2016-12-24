2cf422d7287dc44c51a0e61de098a0c3aa6d2393
webianks <webianks@gmail.com>
2016-12-24 21:42:41 +0530

    Now getting all the SMS in the phone.

--- app/src/main/java/com/webianks/hatkemessenger/MainActivity.java
+++ app/src/main/java/com/webianks/hatkemessenger/MainActivity.java
@@ -1,7 +1,13 @@
 package com.webianks.hatkemessenger;
 
+import android.content.ContentResolver;
+import android.database.Cursor;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,45 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toast.makeText(this,"You have "+getAllSms().size()+" SMS",Toast.LENGTH_LONG).show();
+
+    }
+
+    public List<Sms> getAllSms() {
+
+        List<Sms> lstSms = new ArrayList<Sms>();
+        Sms objSms = new Sms();
+        Uri message = Uri.parse("content://sms/");
+        ContentResolver cr = getContentResolver();
+
+        Cursor c = cr.query(message, null, null, null, null);
+        startManagingCursor(c);
+        int totalSMS = c.getCount();
+
+        if (c.moveToFirst()) {
+            for (int i = 0; i < totalSMS; i++) {
+
+                objSms = new Sms();
+                objSms.setId(c.getString(c.getColumnIndexOrThrow("_id")));
+                objSms.setAddress(c.getString(c
+                        .getColumnIndexOrThrow("address")));
+                objSms.setMsg(c.getString(c.getColumnIndexOrThrow("body")));
+                objSms.setReadState(c.getString(c.getColumnIndex("read")));
+                objSms.setTime(c.getString(c.getColumnIndexOrThrow("date")));
+                if (c.getString(c.getColumnIndexOrThrow("type")).contains("1")) {
+                    objSms.setFolderName("inbox");
+                } else {
+                    objSms.setFolderName("sent");
+                }
+
+                lstSms.add(objSms);
+                c.moveToNext();
+            }
+        } else {
+            //no SMS to show
+        }
+        c.close();
+        return lstSms;
     }
 }
