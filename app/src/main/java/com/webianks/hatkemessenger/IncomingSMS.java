e072cd7eb35797111f32299f4115cad2d7e3d774
webianks <webianks@gmail.com>
2016-12-24 16:24:33 +0530

    base code added for sms_receiver

---
+++ app/src/main/java/com/webianks/hatkemessenger/IncomingSMS.java
@@ -0,0 +1,59 @@
+package com.webianks.hatkemessenger;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.telephony.SmsMessage;
+import android.util.Log;
+import android.widget.Toast;
+
+/**
+ * Created by R Ankit on 24-12-2016.
+ */
+
+public class IncomingSMS extends BroadcastReceiver {
+
+
+    private String TAG = IncomingSMS.class.getSimpleName();
+    // Get the object of SmsManager
+    private final SmsManager sms = SmsManager.getDefault();
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        // Retrieves a map of extended data from the intent.
+        final Bundle bundle = intent.getExtras();
+
+        try {
+            if (bundle != null) {
+
+                final Object[] pdusObj = (Object[]) bundle.get("pdus");
+
+                for (int i = 0; i < pdusObj.length; i++) {
+
+                    SmsMessage currentMessage = SmsMessage.createFromPdu((byte[]) pdusObj[i]);
+                    String phoneNumber = currentMessage.getDisplayOriginatingAddress();
+
+                    String senderNum = phoneNumber;
+                    String message = currentMessage.getDisplayMessageBody();
+
+                    Log.i(TAG, "senderNum: " + senderNum + "; message: " + message);
+
+                    // Show Alert
+                    int duration = Toast.LENGTH_LONG;
+                    Toast toast = Toast.makeText(context,
+                            "senderNum: " + senderNum + ", message: " + message, duration);
+                    toast.show();
+
+                } // end for loop
+            } // bundle is null
+
+        } catch (Exception e) {
+            Log.e(TAG, "Exception smsReceiver" + e);
+
+        }
+
+    }
+}
