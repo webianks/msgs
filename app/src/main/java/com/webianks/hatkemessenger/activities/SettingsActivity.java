622758f1262929c01de5bffb4eb572afac97b6a7
webianks <webianks@gmail.com>
2016-12-27 20:00:29 +0530

    Google drive api configuration done.

--- app/src/main/java/com/webianks/hatkemessenger/activities/SettingsActivity.java
+++ app/src/main/java/com/webianks/hatkemessenger/activities/SettingsActivity.java
@@ -1,20 +1,47 @@
 package com.webianks.hatkemessenger.activities;
 
 
+import android.content.Intent;
+import android.content.IntentSender;
 import android.content.SharedPreferences;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.os.Bundle;
 import android.preference.Preference;
 import android.preference.PreferenceFragment;
 import android.preference.PreferenceManager;
 import android.preference.SwitchPreference;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.MenuItem;
+import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.drive.Drive;
 import com.webianks.hatkemessenger.R;
 import com.webianks.hatkemessenger.constants.Constants;
+import com.webianks.hatkemessenger.utils.Helpers;
 
-public class SettingsActivity extends AppCompatActivity {
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateEncodingException;
+import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
 
+public class SettingsActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener {
+
+
+    private GoogleApiClient mGoogleApiClient;
+    private final int RESOLVE_CONNECTION_REQUEST_CODE = 111;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,8 +53,25 @@ public class SettingsActivity extends AppCompatActivity {
         getFragmentManager().beginTransaction().
                 replace(R.id.container,
                         new MyPreferenceFragment()).commit();
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(Drive.API)
+                .addScope(Drive.SCOPE_APPFOLDER)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
     }
 
+    @Override
+    protected void onActivityResult(final int requestCode, final int resultCode, final Intent data) {
+        switch (requestCode) {
+            case RESOLVE_CONNECTION_REQUEST_CODE:
+                if (resultCode == RESULT_OK) {
+                    mGoogleApiClient.connect();
+                }
+                break;
+        }
+    }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -36,6 +80,39 @@ public class SettingsActivity extends AppCompatActivity {
             finish();
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+
+        Toast.makeText(this, "Connected to drive api", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+    }
+
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+        if (connectionResult.hasResolution()) {
+            try {
+                connectionResult.startResolutionForResult(this, RESOLVE_CONNECTION_REQUEST_CODE);
+            } catch (IntentSender.SendIntentException e) {
+                // Unable to resolve, message user appropriately
+            }
+        } else {
+            GooglePlayServicesUtil.getErrorDialog(connectionResult.getErrorCode(), this, 0).show();
+        }
+
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        startDriveApi();
     }
 
     public static class MyPreferenceFragment extends PreferenceFragment {
@@ -57,12 +134,17 @@ public class SettingsActivity extends AppCompatActivity {
                 public boolean onPreferenceChange(Preference preference, Object o) {
                     //backup dialog
 
+                    //((SettingsActivity) getActivity()).startDriveApi();
+
                     return true;
                 }
             });
-
         }
-
     }
+
+    private void startDriveApi() {
+        mGoogleApiClient.connect();
+    }
+
 
 }
