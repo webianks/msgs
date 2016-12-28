75fb3ccf7ea659b2e303009ec28945bc0dbc9017
webianks <webianks@gmail.com>
2016-12-29 01:30:49 +0530

    Toggling the googleApiClient connect/disconnect

--- app/src/main/java/com/webianks/hatkemessenger/activities/SettingsActivity.java
+++ app/src/main/java/com/webianks/hatkemessenger/activities/SettingsActivity.java
@@ -1,9 +1,9 @@
 package com.webianks.hatkemessenger.activities;
 
-import android.content.Context;
 import android.content.Intent;
 import android.content.IntentSender;
 import android.content.SharedPreferences;
+import android.nfc.Tag;
 import android.os.Bundle;
 import android.preference.Preference;
 import android.preference.PreferenceFragment;
@@ -15,13 +15,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.MenuItem;
 import android.widget.Toast;
+
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.ResultCallback;
 import com.google.android.gms.drive.Drive;
 import com.google.android.gms.drive.DriveApi;
-import com.google.android.gms.drive.DriveContents;
 import com.google.android.gms.drive.DriveFile;
 import com.google.android.gms.drive.DriveFolder;
 import com.google.android.gms.drive.DriveId;
@@ -29,12 +29,7 @@ import com.google.android.gms.drive.MetadataChangeSet;
 import com.webianks.hatkemessenger.R;
 import com.webianks.hatkemessenger.constants.Constants;
 import com.webianks.hatkemessenger.tasks.EditContentsAsyncTask;
-import com.webianks.hatkemessenger.tasks.ApiClientAsyncTask;
 import com.webianks.hatkemessenger.tasks.RetrieveDriveFileContentsAsyncTask;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 
 public class SettingsActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks,
         GoogleApiClient.OnConnectionFailedListener {
@@ -107,7 +102,7 @@ public class SettingsActivity extends AppCompatActivity implements GoogleApiClie
                 @Override
                 public void onResult(DriveFolder.DriveFileResult result) {
                     if (!result.getStatus().isSuccess()) {
-                        showMessage("Error while trying to create the file "+result.getStatus());
+                        showMessage("Error while trying to create the file " + result.getStatus());
                         return;
                     }
 
@@ -195,12 +190,12 @@ public class SettingsActivity extends AppCompatActivity implements GoogleApiClie
 
     }
 
-    @Override
+    /*@Override
     protected void onStart() {
         super.onStart();
         startDriveApi();
     }
-
+*/
     public GoogleApiClient getGoogleApiClient() {
         return mGoogleApiClient;
     }
@@ -208,6 +203,7 @@ public class SettingsActivity extends AppCompatActivity implements GoogleApiClie
     public static class MyPreferenceFragment extends PreferenceFragment {
 
         private SwitchPreference drivePref;
+        private String TAG = MyPreferenceFragment.class.getSimpleName();
 
         @Override
         public void onCreate(Bundle savedInstanceState) {
@@ -224,12 +220,23 @@ public class SettingsActivity extends AppCompatActivity implements GoogleApiClie
                 public boolean onPreferenceChange(Preference preference, Object o) {
                     //backup dialog
 
-                    //((SettingsActivity) getActivity()).startDriveApi();
+                    SwitchPreference switchPreference = (SwitchPreference) preference;
+
+                    if (!switchPreference.isChecked()) {
+                        Log.d(TAG, "isChecked");
+                        ((SettingsActivity) getActivity()).startDriveApi();
+                    } else {
+                        ((SettingsActivity) getActivity()).disconnectApi();
+                    }
 
                     return true;
                 }
             });
         }
+    }
+
+    private void disconnectApi() {
+        mGoogleApiClient.disconnect();
     }
 
     private void startDriveApi() {
