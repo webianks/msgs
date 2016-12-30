4f22fc930f85d05b53912311fec6e840b6e193ab
webianks <webianks@gmail.com>
2016-12-30 14:37:25 +0530

    Added delivery receiver

---
+++ app/src/main/java/com/webianks/hatkemessenger/receivers/SMSDeliverReceiver.java
@@ -0,0 +1,31 @@
+package com.webianks.hatkemessenger.receivers;
+
+import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+import com.webianks.hatkemessenger.R;
+
+/**
+ * Created by R Ankit on 30-12-2016.
+ */
+
+public class SMSDeliverReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent arg1) {
+        switch (getResultCode()) {
+            case Activity.RESULT_OK:
+                Toast.makeText(context, R.string.sms_delivered,
+                        Toast.LENGTH_SHORT).show();
+                break;
+            case Activity.RESULT_CANCELED:
+                Toast.makeText(context, R.string.sms_not_delivered,
+                        Toast.LENGTH_SHORT).show();
+                break;
+        }
+
+    }
+}
